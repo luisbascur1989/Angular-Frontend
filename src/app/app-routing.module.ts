f01debe66cb58f8631c835d9d562515f2b5a3c9a
christophervenegas2 <christophervenegas2@gmail.com>
2021-03-10 22:35:36 -0300

    Nuevas rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './modules/create/create.component';
 import { HomeComponent } from './modules/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'home'
-    },
-    {
-      path: 'home',
-      component: HomeComponent,
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'create',
+    component: CreateComponent,
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
